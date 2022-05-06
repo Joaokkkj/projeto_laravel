3049dafd044267e6e5af3d386629beecf59093e9
João Pedro de Oliveira Ferreira <joaopedro77ferreira@gmail.com>
2022-05-06 08:46:41 -0300

    alterando rotas

--- petec/app/Http/Controllers/AboutController.php
+++ petec/app/Http/Controllers/AboutController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     public function index(){
-        return "Olá mundo.. Segue o líder....";
+        return view("about");
     }
 }
